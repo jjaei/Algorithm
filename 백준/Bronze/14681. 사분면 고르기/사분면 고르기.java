85e16fa0fbe76e6de9b40c817417be89a634621d
Kwon Jaei <120344687+kwonjaei@users.noreply.github.com>
2023-02-27 18:43:51 +0900

    [Bronze V] Title: 사분면 고르기, Time: 108 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+	int x=0;
+	int y=0;
+	
+	Scanner sc = new Scanner(System.in);
+	x = sc.nextInt();
+	y = sc.nextInt();
+	
+	if(x>=1 && y>=1) {
+	   System.out.println(1); 	
+	} else if(x<0 && y>=1) {
+		System.out.println(2);
+	} else if(x<0 && y<0) {
+		System.out.println(3);
+	} else if(x>=1 && y<0) {
+		System.out.println(4);
+	}
+  }		
+}
